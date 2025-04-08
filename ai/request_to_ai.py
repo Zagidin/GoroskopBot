2c724c1d6e79a89af2771882141913403877219e
Zagidin <prap0235@gmail.com>
2025-04-09 01:27:59 +0300

    Zagidin

--- ai/request_to_ai.py
+++ ai/request_to_ai.py
@@ -4,7 +4,6 @@
 
 from os import getenv
 from openai import OpenAI
-# from random import randint
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,22 +13,25 @@ client = OpenAI(
   api_key=getenv("API_KEY_AI"),
 )
 
-# free_ai = [
-#   "cognitivecomputations/dolphin3.0-mistral-24b:free", "cognitivecomputations/dolphin3.0-r1-mistral-24b:free",
-#   "google/gemini-2.0-pro-exp-02-05:free", "google/gemini-2.0-flash-lite-preview-02-05:free",
-#   "qwen/qwen2.5-vl-72b-instruct:free", "deepseek/deepseek-r1-distill-llama-70b:free",
+# free_ai = {
+#   "cognitivecomputations/dolphin3.0-mistral-24b:free",
+#   "cognitivecomputations/dolphin3.0-r1-mistral-24b:free",
+#   "google/gemini-2.0-pro-exp-02-05:free",
+#   "google/gemini-2.0-flash-lite-preview-02-05:free",
+#   "qwen/qwen2.5-vl-72b-instruct:free",
+#   "deepseek/deepseek-r1-distill-llama-70b:free",
 #   "google/gemini-2.0-flash-lite-preview-02-05:free"
-# ]
+# }
 
 def request_to_ai(message_content):
   """
-    models:
-        1) cognitivecomputations/dolphin3.0-mistral-24b:free 🚩
-        2) cognitivecomputations/dolphin3.0-r1-mistral-24b:free ✅ шо то не робит лимит запросов скорее всего
+    models FREE AI:
+        1) cognitivecomputations/dolphin3.0-mistral-24b:free
+        2) cognitivecomputations/dolphin3.0-r1-mistral-24b:free
         3) google/gemini-2.0-pro-exp-02-05:free
         4) google/gemini-2.0-flash-lite-preview-02-05:free
         5) deepseek/deepseek-r1-distill-llama-70b:free
-        6) qwen/qwen2.5-vl-72b-instruct:free ✅🚩 <- норм справляется
+        6) qwen/qwen2.5-vl-72b-instruct:free ✅
   """
   completion = client.chat.completions.create(
     model="qwen/qwen2.5-vl-72b-instruct:free", # free_ai[randint(0, len(free_ai))]
@@ -42,6 +44,3 @@ def request_to_ai(message_content):
   )
 
   return completion.choices[0].message.content
-
-
-# print(request_to_ai("Прием Прием!"))
